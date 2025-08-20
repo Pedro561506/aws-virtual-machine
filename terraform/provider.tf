58681bb7bb11357b090a81b2ffd113842525b584
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 20:26:15 -0300

    teste1

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.12.2"
+  required_version = "~> 1.12.1"
 
   required_providers {
     aws = {
@@ -12,9 +12,9 @@ terraform {
 
 }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
-}
+ backend "s3" {
+    bucket       = "aws-s3-tfstate-<vasco1>"  # SUBSTITUA
+    key          = "tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
---
+++ terraform/vars.tf
@@ -0,0 +1,3 @@
+variable "bucket_name" {
+  default = "aws-s3-<vasco1>"  # SUBSTITUA
+}
